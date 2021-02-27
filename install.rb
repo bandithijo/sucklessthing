9d447f54e960ab5b37cd68b2e93dc8679bcc833b
Rizqi Nur Assyaufi <bandithijo@gmail.com>
2021-02-27 18:17:20 +0800

    Add dwm dual status

--- install.rb
+++ install.rb
@@ -1,5 +1,8 @@
 #!/usr/bin/env ruby
 
+# TODO:
+# 1. Solve dependecy list
+
 local_path = '~/.local/bin'
 system "mkdir -p #{local_path}"
 system "find suck* \\( ! -name suckmerge \\) | cpio -dumpv #{local_path}"
