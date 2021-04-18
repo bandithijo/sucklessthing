4953bc7ade1ab5292d506a9da7152ad81dec0a8a
Rizqi Nur Assyaufi <bandithijo@gmail.com>
2021-04-18 16:37:49 +0800

    rename suckmerge2 to suckmerge

--- install.rb
+++ install.rb
@@ -1,9 +1,10 @@
 #!/usr/bin/env ruby
 
 # TODO:
-# 1. Solve dependecy list
+# 1. Solve PATH definition
 
 local_path = '~/.local/bin'
 system "mkdir -p #{local_path}"
 system "find suck* \\( ! -name suckmerge \\) | cpio -dumpv #{local_path}"
+system "cp suck* #{local_path}"
 puts '=> Installation COMPLETED!'
